6005744708098b38dfdb878b47e99dc35781349d
Dan Puiatti <dpuiatti@gmail.com>
2019-01-28 18:35:26 -0500

    Updates

---
+++ precache-manifest.b81a39cb246d5a5531172f0fa6efd42f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da3d8140e097582c686c2b72bd80d582",
+    "url": "/Danskii/Toronto-Bike-Fixit-Map/static/media/react-share-pin-example.da3d8140.png"
+  },
+  {
+    "revision": "6ceabcc9177e7a75bb64",
+    "url": "/Danskii/Toronto-Bike-Fixit-Map/static/js/runtime~main.6ceabcc9.js"
+  },
+  {
+    "revision": "b1e6dbfcff16e40562c7",
+    "url": "/Danskii/Toronto-Bike-Fixit-Map/static/js/main.b1e6dbfc.chunk.js"
+  },
+  {
+    "revision": "52700367582d91f089e3",
+    "url": "/Danskii/Toronto-Bike-Fixit-Map/static/js/1.52700367.chunk.js"
+  },
+  {
+    "revision": "b1e6dbfcff16e40562c7",
+    "url": "/Danskii/Toronto-Bike-Fixit-Map/static/css/main.427a9a49.chunk.css"
+  },
+  {
+    "revision": "fcc9af5e023ed363be0bff67279218de",
+    "url": "/Danskii/Toronto-Bike-Fixit-Map/index.html"
+  }
+];
