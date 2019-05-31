bba970b8faec7c42fde0ad567f677ce4a2761370
Daniel Puiatti <puiattid@corp01.oct.on.ca>
2019-05-31 09:16:21 -0400

    Updates

---
+++ precache-manifest.fd0fc65e7f67b0248e99f0689f9c69c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213a942092a6c13ddd59edd5967a4fa9",
+    "url": "/Toronto-Bike-Fixit-Map/index.html"
+  },
+  {
+    "revision": "4467f665064c067279fc",
+    "url": "/Toronto-Bike-Fixit-Map/static/css/main.39054b92.chunk.css"
+  },
+  {
+    "revision": "85c6bb5a3836d2d5f50e",
+    "url": "/Toronto-Bike-Fixit-Map/static/js/2.b2b57223.chunk.js"
+  },
+  {
+    "revision": "4467f665064c067279fc",
+    "url": "/Toronto-Bike-Fixit-Map/static/js/main.aad577ed.chunk.js"
+  },
+  {
+    "revision": "0e88c240db779f59f879",
+    "url": "/Toronto-Bike-Fixit-Map/static/js/runtime~main.63ba756e.js"
+  },
+  {
+    "revision": "fcb1692c32e9335e653f644cfb13d144",
+    "url": "/Toronto-Bike-Fixit-Map/static/media/symphony.fcb1692c.png"
+  }
+]);
