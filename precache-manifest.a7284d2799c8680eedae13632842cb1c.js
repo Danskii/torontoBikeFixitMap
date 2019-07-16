516785a776ce83be908bd42c77dca01fe77ad1bc
Daniel Puiatti <puiattid@corp01.oct.on.ca>
2019-07-16 10:30:52 -0400

    Updates

---
+++ precache-manifest.a7284d2799c8680eedae13632842cb1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d345151d9ddf0892ceda57ac99171971",
+    "url": "/Toronto-Bike-Fixit-Map/index.html"
+  },
+  {
+    "revision": "bb4a884ca932c5759ef8",
+    "url": "/Toronto-Bike-Fixit-Map/static/css/main.c325eaa1.chunk.css"
+  },
+  {
+    "revision": "85c6bb5a3836d2d5f50e",
+    "url": "/Toronto-Bike-Fixit-Map/static/js/2.b2b57223.chunk.js"
+  },
+  {
+    "revision": "bb4a884ca932c5759ef8",
+    "url": "/Toronto-Bike-Fixit-Map/static/js/main.775346f5.chunk.js"
+  },
+  {
+    "revision": "0e88c240db779f59f879",
+    "url": "/Toronto-Bike-Fixit-Map/static/js/runtime~main.63ba756e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Toronto-Bike-Fixit-Map/precache-manifest.7e7a540e5d4b7a7370680f3ab060245b.js"
+  "/Toronto-Bike-Fixit-Map/precache-manifest.a7284d2799c8680eedae13632842cb1c.js"
 );
 
 self.addEventListener('message', (event) => {
